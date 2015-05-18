335739aaf45e6568649c19f084b9ad1493550578
brunofornazari <bruno_fornazari@hotmail.com>
2015-05-18 17:49:11 -0300

    up

--- app/src/main/java/com/example/bruno/myapplication/update_image.java
+++ app/src/main/java/com/example/bruno/myapplication/update_image.java
@@ -37,8 +37,8 @@ public class update_image extends ActionBarActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == REQUEST_IMAGE_CAMERA && requestCode == RESULT_OK){
             Bundle extras = data.getExtras();
-            btimg = (Bitmap) extras.get("data");
-            imageview.setImageBitmap(btimg);
+            Bitmap photo = (Bitmap) extras.get("data");
+            imageview.setImageBitmap(photo);
         }
     }
 
@@ -57,10 +57,10 @@ public class update_image extends ActionBarActivity {
         setContentView(R.layout.activity_update_image);
 
         imageview = (ImageView) findViewById(R.id.image_view_result);
-        //Drawable imgsrc = getResources().getDrawable(R.drawable.effect_icon);
-        //btimg = ((BitmapDrawable) imgsrc).getBitmap();
-        //imageview.setImageBitmap(btimg);
-        launchCamera(new View(this));
+        Drawable imgsrc = getResources().getDrawable(R.drawable.effect_icon);
+        btimg = ((BitmapDrawable) imgsrc).getBitmap();
+        imageview.setImageBitmap(btimg);
+
         FrameLayout invert = (FrameLayout)findViewById(R.id.ivtImage);
         invert.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,7 +68,7 @@ public class update_image extends ActionBarActivity {
                 invertImage(btimg);
             }
         });
-
+        //launchCamera(new View(this));
     }
 
     @Override
